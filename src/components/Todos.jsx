b052e6b842aa34a52ba8b2d86d2d95254a00c2b5
Hossein Farahkordmahaleh <h.farah61@gmail.com>
2024-12-08 18:47:53 +0200

    seach

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { useNavigate,Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 const Todos = () => {
   const navigate = useNavigate();
@@ -14,7 +14,6 @@ const Todos = () => {
     });
   }, []);
 
-
   const deleteTodo = (id) => {
     if (!window.confirm("Are you sure you want to delete this todo?")) {
       return;
@@ -23,7 +22,19 @@ const Todos = () => {
     axios.delete(`http://localhost:5000/todos/${id}`).then(() => {
       setTodos(todos.filter((todo) => todo.id !== id));
     });
-  }
+  };
+
+  const search = (e) => {
+    const query = e.target.value.toLowerCase();
+    axios.get(`http://localhost:5000/todos`).then((response) => {
+      const filteredTodos = response.data.filter(
+        (todo) =>
+          todo.title.toLowerCase().includes(query) ||
+          todo.status.toLowerCase().includes(query)
+      );
+      setTodos(filteredTodos);
+    });
+  };
 
   return (
     <div className="container-fluid w-100 mt-5">
@@ -31,11 +42,23 @@ const Todos = () => {
         <h1>Loading...</h1>
       ) : (
         <>
-        <h1 className="text-center mt-2 mb-5">Todos</h1>
-        <Link to="/new" className="btn btn-primary mb-3"><i className="fas fa-plus"></i> Add New Todo</Link>
-          <div className="row d-flex justify-content-center">
+          <h1 className="text-center mt-2 mb-5">Todos</h1>
+          <div className="row d-flex mb-3">
+            <Link to="/new" className="btn btn-primary mb-2 ms-4 w-25">
+              <i className="fas fa-plus"></i> Add New Todo
+            </Link>
+            <form className="d-flex w-100">
+              <input
+                type="search"
+                className="form-control me-2"
+                placeholder="Search Todos"
+                onChange={search}
+              />
+            </form>
+          </div>
+          <div className="row row-cols-1 row-cols-sm-1 row-cols-md-1 row-cols-lg-1">
             {todos.map((todo) => (
-              <div key={todo.id} className="col-12 col-sm-6 col-md-4 col-lg-6 mb-4">
+              <div key={todo.id} className="col mb-4">
                 <div className="card h-100">
                   <div
                     className={`card-header ${
@@ -49,15 +72,24 @@ const Todos = () => {
                     {todo.title}
                   </div>
                   <div className="card-body">
-                    <h5 className="card-title">{todo.title}</h5>
+                    <h5 className="card-title"> {todo.title}</h5>
                   </div>
-                  <div className="card-footer">
-                    <p>{todo.status}</p>
-                    <button className="btn btn-warning" onClick={() => navigate(`/todo/${todo.id}`)}><i className="fas fa-eye"></i></button>
-                    <button className="btn btn-danger ms-1" onClick={() => deleteTodo(todo.id)}>
-                      <i className="fas fa-trash"></i>
-                    </button>
-
+                  <div className="card-footer d-flex justify-content-between">
+                    <p className="mb-0">{todo.status}</p>
+                    <div>
+                      <button
+                        className="btn btn-warning me-1"
+                        onClick={() => navigate(`/todo/${todo.id}`)}
+                      >
+                        <i className="fas fa-eye"></i>
+                      </button>
+                      <button
+                        className="btn btn-danger"
+                        onClick={() => deleteTodo(todo.id)}
+                      >
+                        <i className="fas fa-trash"></i>
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
